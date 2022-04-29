539a3b5f51215fb5f1e5d301a2cf397e79da0556
JACKSON RAFAEL DE JESUS VIANA <jackson.rjviana@senacsp.edu.br>
2022-04-28 22:18:40 -0300

    aula-28-04-22

--- exemploForm/listar.php
+++ exemploForm/listar.php
@@ -3,16 +3,17 @@ $f = fopen('bancodedados.csv','r');
 
 echo "<a href='index.php'>Voltar</a>";
 
-echo "<table boarder='1'>
+echo "<table border='1'>
         <tr>
             <td>Nome</td><td>Repetições</td>
         </tr>";
-while($linha = fgets($f)){
+
+while( $linha = fgets($f) ){
 
     $campos = explode(';', $linha);
     $nome = $campos[0];
     $rep = $campos[1];
-    
+
     echo "  <tr>
                 <td>$nome</td><td>$rep</td>
             </tr>";
--- exemploForm/recebe.php
+++ exemploForm/recebe.php
@@ -11,4 +11,10 @@ for($i = 0 ; $i < $repeticao && $repeticao < 1000 ; $i++ ){
     //if ($i > 1000) break;
 }
 
+//grava no arquivo
+$f = fopen('bancodedados.csv','a');
+fwrite($f, "$usuario;$repeticao\r\n");
+fclose($f);
+
+
 echo "FIM";
